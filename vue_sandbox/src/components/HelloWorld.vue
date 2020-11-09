98108d59d1d71fec037a418b0a0392db6e167818
raiti-chan <rusuke@t-net.ne.jp>
2020-11-09 22:47:11 +0900

    ルーターリンクの追加

--- vue_sandbox/src/components/HelloWorld.vue
+++ vue_sandbox/src/components/HelloWorld.vue
@@ -6,6 +6,12 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
+    <h3>User's Page</h3>
+    <ul>
+      <li>
+        <router-link to="/userPage/raitichan">Raitichan</router-link>
+      </li>
+    </ul>
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
@@ -47,14 +53,17 @@ export default class HelloWorld extends Vue {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
