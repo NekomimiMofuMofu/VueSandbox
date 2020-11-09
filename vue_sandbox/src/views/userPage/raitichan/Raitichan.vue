879912bbda3692979db915e5cab54aa9d3ee7f93
raiti-chan <rusuke@t-net.ne.jp>
2020-11-09 22:46:39 +0900

    ユーザーページの追加

---
+++ vue_sandbox/src/views/userPage/raitichan/Raitichan.vue
@@ -0,0 +1,22 @@
+﻿<template>
+  <div class="user-raitichan">
+    <h1>Welcome to Raiti's Page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Raitichan extends Vue {
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
