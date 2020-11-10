f374c4e89fc5535dfaff33d54c356c8193d96542
teba3 <k019c1158@it-neec.jp>
2020-11-10 15:47:10 +0900

    ユーザーページの追加

---
+++ vue_sandbox/src/views/userPage/igarashi/Igarashi.vue
@@ -0,0 +1,22 @@
+﻿<template>
+  <div class="user-teba3">
+    <h1>五十嵐</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Igarashi extends Vue {
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
