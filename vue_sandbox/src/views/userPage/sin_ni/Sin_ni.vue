571577c6ae61984c938ba6507d94ffe8ca040062
sin_ni <0bl6zqh7li@gmail.com>
2020-11-11 13:31:44 +0900

    sin_niのユーザーページを作成

---
+++ vue_sandbox/src/views/userPage/sin_ni/Sin_ni.vue
@@ -0,0 +1,22 @@
+﻿<template>
+  <div class="user-sin_ni">
+    <h1>Welcome to Sinni's Page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Sinni extends Vue {
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
