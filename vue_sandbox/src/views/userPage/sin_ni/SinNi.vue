1e38975dd23591b9a841cf13b7992273c7ad0603
sin_ni <0bl6zqh7li@gmail.com>
2020-11-16 11:28:05 +0900

    なんかわからんけどユーザーページもう一回追加するよ

---
+++ vue_sandbox/src/views/userPage/sin_ni/SinNi.vue
@@ -0,0 +1,22 @@
+﻿<template>
+  <div class="user-sin_ni">
+    <h1>Sin_niだよぉぉぉぉ</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class SinNi extends Vue {
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
